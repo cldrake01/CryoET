6f3bb42fd1b5aac04e1fc7201e5ba960aee7d88b
cldrake01 <collinlindendrake@gmail.com>
2024-11-18 12:23:18 -0700

    ...

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import numpy as np
 z = zarr.array(np.random.rand(100, 100), chunks=(10, 10))
 
 # Store the array on disk
-zarr.save("my_array.zarr", z)
+path = "CryoET Object Identification/test/static/ExperimentRuns/TS_5_4/VoxelSpacing10.000"
+zarr.save(path, z)
 
-z = zarr.open("my_array.zarr", mode="r")
+z = zarr.open(path, mode="r")
 print(z.shape)
 print(z[:10, :10])
